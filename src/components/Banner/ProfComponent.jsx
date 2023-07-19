025d5c180f9f5c9b594cf1d1c784a47bbd5ee759
gokuls456 <gokulduki23@gmail.com>
2023-07-19 13:32:54 -0700

    icon removed

--- src/components/Banner/ProfComponent.jsx
+++ src/components/Banner/ProfComponent.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { AiOutlineInstagram } from "react-icons/ai";
-import { GiEarthAmerica } from "react-icons/gi";
 import { FaLinkedinIn } from "react-icons/fa";
 import { Slide } from "react-awesome-reveal";
 
